05d0feee2a602f4dd5c29a6a58ae8071d9161264
Duzie Uche-Abba <72194611+duzzisantos@users.noreply.github.com>
2022-05-19 18:50:06 +0200

    Delete .history directory

--- .history/rentify-server/config/config_20220519182558.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: "mongodb+srv://ucheabba:barcelonaG22@cluster0.1s7zq.mongodb.net/rentify?retryWrites=true&w=majority"
-}
-
--- .history/rentify-server/config/config_20220519184444.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //add your URL here
-}
-
